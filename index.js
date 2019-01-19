2c5cde94babdab3adf18d3183cde5e63aa8b932c
Dylan <34420038+DPM97@users.noreply.github.com>
2019-01-18 19:42:11 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
 const delay = require('delay');
 const cheerio = require('cheerio')
 
-let accounts = ['hopresellgame@gmail.com:Winslow65one', 'dylanmaloy97@gmail.com:Winslow65one'];
+let accounts = [];
 
 
 function createMain() {
@@ -26,6 +26,21 @@ ipcMain.on('stop', function () {
     app.quit();
 });
 
+ipcMain.on('add', (event, arg, arg2) => {
+    let string = arg + ':' + arg2;
+    if (arg == ':' || arg.includes('@gmail.com') == 0 || arg == '' || arg2 == '') {
+        console.log("Account not formatted correctly");
+    } else {
+        accounts.push(string);
+        console.log(accounts);
+    }
+});
+
+ipcMain.on('clear', function () {
+    console.log('cleared emails');
+    accounts = [];
+});
+
 function startTask() {
     for (var i = 0; i < accounts.length; i++) {
         let arr = accounts[i].split(':');
@@ -38,7 +53,7 @@ async function login(arr) {
         headless: false,
         args: [
             '--window-size=0,0'
-          ]
+        ]
     });
     const page = await browser.newPage();
     await page.goto('https://accounts.google.com/signin/v2/identifier?hl=en&service=youtube&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Ffeature%3Dsign_in_button%26hl%3Den%26app%3Ddesktop%26next%3D%252F%26action_handle_signin%3Dtrue&passive=true&uilel=3&flowName=GlifWebSignIn&flowEntry=ServiceLogin');
@@ -51,7 +66,7 @@ async function login(arr) {
     let cookies = await page.cookies();
     console.log(arr[0] + ' successfully logged in')
     browser.close();
-    setInterval( () => {
+    setInterval(() => {
         getVid(cookies);
         getSearch(cookies);
     }, 20000);
@@ -72,7 +87,7 @@ async function getVid(cookies) {
     let link = $('#video-title').attr('href');
     await page.goto('http://youtube.com' + link);
     console.log('http://youtube.com' + link);
-    setTimeout( () => {
+    setTimeout(() => {
         browser.close();
     }, 15000);
 
@@ -88,9 +103,9 @@ async function getSearch(cookies) {
         await page.setCookie(cookies[i]);
     }
 
-    let common = ["the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","I","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see","number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","part"];
+    let common = ["the", "of", "and", "a", "to", "in", "is", "you", "that", "it", "he", "was", "for", "on", "are", "as", "with", "his", "they", "I", "at", "be", "this", "have", "from", "or", "one", "had", "by", "word", "but", "not", "what", "all", "were", "we", "when", "your", "can", "said", "there", "use", "an", "each", "which", "she", "do", "how", "their", "if", "will", "up", "other", "about", "out", "many", "then", "them", "these", "so", "some", "her", "would", "make", "like", "him", "into", "time", "has", "look", "two", "more", "write", "go", "see", "number", "no", "way", "could", "people", "my", "than", "first", "water", "been", "call", "who", "oil", "its", "now", "find", "long", "down", "day", "did", "get", "come", "made", "may", "part"];
 
-    await page.goto('https://www.google.com/')  
+    await page.goto('https://www.google.com/')
     let data = common[getRandomInt(100)] + ' ' + common[getRandomInt(100)];
     await page.type('input.gsfi', data);
     await page.keyboard.press('Enter');
@@ -101,5 +116,5 @@ async function getSearch(cookies) {
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
-  }
-  
+}
+
